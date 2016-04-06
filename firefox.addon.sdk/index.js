b238678d6639f7ee21c2f38c45bdc6731be3481b
刘鑫伟 <liuxinwei1995@gmail.com>
2016-04-06 17:19:45 +0800

    实现代码注入
    JS代码注入listen

--- firefox.addon.sdk/index.js
+++ firefox.addon.sdk/index.js
@@ -198,5 +198,59 @@ function sideBar() {
   
   function install() {}
   
-windowListener.register();  
+  windowListener.register();  
+}
+
+function listen() {
+   
+  var {Ci, Cu} = require("chrome");  
+  Cu.import('resource://gre/modules/Services.jsm');
+
+  var citeXplore = {
+    
+    init: function() {
+      var window = require("sdk/window/utils").getMostRecentBrowserWindow();
+        window.removeEventListener("load", citeXplore.init, true);
+        
+        var appcontent = window.document.getElementById("appcontent");
+        if (appcontent) {
+            appcontent.addEventListener("DOMContentLoaded", citeXplore.onPageLoad, false);
+        }
+
+    },
+
+    onPageLoad: function(aEvent) {
+        console.log("onPageLoad");
+        var urls_reg = "^(https*:\/\/xueshu\.baidu\.com\/.*)";
+        var doc = aEvent.originalTarget;
+        var m = (new RegExp(urls_reg, 'i')).test(doc.location.href);
+        
+        var href = doc.location.href;
+        var keyWord = href.substring(href.indexOf("wd=")+3, href.indexOf("&tn"));
+        //alert(keyWord);
+        
+        if (!m) return;
+        
+        
+        var data = doc.createElement("script");
+            data.id = "citeXplore";
+            data.innerHTML = "function getUrl(i){var data = document.getElementById(i);var allSourceUrl = new Array(); var source = data.getElementsByClassName(\"sc_allversion\")[0].getElementsByClassName(\"v_item_span\");for (var i=0; i<source.length; i++) {allSourceUrl[i] = source[i].getElementsByTagName('a')[0].innerHTML + \"**http://xueshu.baidu.com\" + source[i].getElementsByTagName('a')[0].getAttribute(\"href\");}; alert(allSourceUrl[0]);}"
+            
+            doc.getElementsByTagName('body')[0].appendChild(data);
+            
+            var list = doc.getElementsByClassName('result sc_default_result xpath-log');
+            
+            for (var i=0; i<list.length; i++) {
+                var button = doc.createElement("span");
+                button.innerHTML ="<input type=\"button\" value=\"getData\" onclick=\"getUrl(" + (i+1) + ")\">";
+                button.id="getUrl" + (i+1);
+                list[i].appendChild(button);
+            } 
+
+         sideBar();
+        // citeXplorePanel.show();
+    }
+  };
+
+citeXplore.init();
 }
