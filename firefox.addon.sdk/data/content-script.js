4ca2c34be5fce9d91b363c023d69c2e648bd6fd8
刘鑫伟 <liuxinwei1995@gmail.com>
2016-04-13 18:22:48 +0800

    change to jpm

---
+++ firefox.addon.sdk/data/content-script.js
@@ -0,0 +1,26 @@
+self.port.emit('load', window.location.href);
+
+if (document.getElementsByClassName("t c_font")[0] != null) {
+    var tCFonts = document.getElementsByClassName("t c_font");
+   
+    for (var i=0; i<tCFonts.length; i++) {
+      tCFonts[i].childNodes[0].addEventListener("click", function() {
+         var data = new Array();
+         data[0] = window.location.href;
+         data[1] = this.innerHTML;
+         data[2] = this.href;
+         var urls = new Array();
+         var source = this.parentNode.parentNode.getElementsByClassName('sc_allversion')[0].getElementsByClassName('v_item_span');
+         
+         for (var j=0; j<source.length; j++) {
+            urls[j] = source[j].getElementsByTagName('a')[0].getAttribute('href');
+         };
+         
+         data[3] = urls;
+         self.port.emit('click', data);
+        });
+    }
+}
+
+
+
