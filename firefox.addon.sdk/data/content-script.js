9a9e1a8ae3dc3696e7652cf4df5fc7b401211294
刘鑫伟 <liuxinwei1995@gmail.com>
2016-04-15 12:01:08 +0800

    change the location of dealing with data

--- firefox.addon.sdk/data/content-script.js
+++ firefox.addon.sdk/data/content-script.js
@@ -4,17 +4,26 @@ if (document.getElementsByClassName("t c_font")[0] != null) {
     for (var i=0; i<tCFonts.length; i++) {
       tCFonts[i].childNodes[0].addEventListener("click", function() {
          var data = new Array();
-         data[0] = window.location.href;
-         data[1] = this.innerHTML;
-         data[2] = this.href;
+         data[1] = window.location.href;
+         data[2] = this.innerHTML.replace(new RegExp("<em>","gm"),"").replace(new RegExp("<\/em>","gm"),"");
+         data[3] = this.href;
+
+         if (data[1].indexOf("&rsv_bp") < data[1].indexOf("&tn")) {
+            data[0] = data[1].substring(data[1].indexOf("wd=") + 3, data[1].indexOf("&rsv_bp"));
+         } else {
+            data[0] = data[1].substring(data[1].indexOf("wd=") + 3, data[1].indexOf("&tn"));
+         }
+         
          var urls = new Array();
          var source = this.parentNode.parentNode.getElementsByClassName('sc_allversion')[0].getElementsByClassName('v_item_span');
-         
          for (var j=0; j<source.length; j++) {
             urls[j] = source[j].getElementsByTagName('a')[0].getAttribute('href');
+            if (urls[j].indexOf("http:") < 0) {
+                urls[j] = "http://xueshu.baidu.com" + urls[j];
+            }
          };
          
-         data[3] = urls;
+         data[4] = urls;
          self.port.emit('click', data);
         });
     }
